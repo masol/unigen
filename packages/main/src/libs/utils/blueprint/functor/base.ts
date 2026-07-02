c8a9b8f2ec578120f25c3f06f53485d1789c0de4
masol <masol.li@gmail.com>
2026-07-02 21:41:09 +0800

    functor中支持了getIOInfo来获取输入/输出并验证是否expired

--- packages/main/src/libs/utils/blueprint/functor/base.ts
+++ packages/main/src/libs/utils/blueprint/functor/base.ts
@@ -3,36 +3,9 @@ import { Capability } from "$types/blueprint/capability.js";
 import { IRunnerContext } from "$types/blueprint/context.js";
 import { ICapaFunctor } from "./type.js";
 
-
-// function getOutputValue(output: any, idx: number): any {
-//     if (Array.isArray(output)) {
-//         return output.length > idx ? output[idx] : null;
-//     }
-
-//     if (idx === 0) return output;
-//     return null;
-// }
-
 export abstract class BaseFunctor implements ICapaFunctor {
 
     constructor(readonly capa: Capability) { }
     // 由run实现方主动调用基类的getInput/saveOutput方法来获取输入和保存输出。
     abstract run(ctx: IRunnerContext): Promise<void>;
-
-
-    // protected saveToOutput(prjDB: PrjDB, output: CapaIOType, value: any): void {
-    //     const key = this.getFieldkey(output);
-    //     if (key) {
-    //         prjDB.set(key, value);
-    //     }
-    // }
-
-    // public saveOutput(ctx: IRunnerContext, output: unknown): void {
-    //     const prjDB: PrjDB = PrjDB.ensure(ctx.prj);
-    //     this.capa.output?.forEach((o, idx) => {
-    //         const ov = getOutputValue(output, idx)
-    //         this.saveToOutput(prjDB, o, ov)
-    //     })
-    // }
-
 }
