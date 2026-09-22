b481a2657afdc8d5925399caf78486a480c1b1d3
masol <masol.li@gmail.com>
2026-09-22 20:06:52 +0800

    未给出输入时，直接使用旧的weave来执行通用任务

--- packages/main/src/libs/common/index.ts
+++ packages/main/src/libs/common/index.ts
@@ -1,5 +1,7 @@
 // run.ts
+import { getInput } from "$libs/blueprint/glossary/input.js";
 import { throwUnprcessable } from "$libs/utils/err.js";
+import { run as runWeave } from '$libs/weave/index.js';
 import type { IRunnerContext } from "$types/blueprint/context.js";
 import { agentsManager } from "./agents/manager.js";
 import { createCommonContext } from "./context.js";
@@ -7,6 +9,10 @@ import { createCommonContext } from "./context.js";
 export async function run(ctx: IRunnerContext): Promise<void> {
     // Logger.debug("response to cmd", ctx.cmd?.body)
     if (!ctx.cmd?.body) {
+        const input = getInput(ctx, "script");
+        if (input.length > 0) {
+            return await runWeave(ctx);
+        }
         throwUnprcessable("本次进入目标循环，但是未指定任意新的内容。")
     }
 
